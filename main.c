cbce1b3d724b8e29ef2cdc37bd78bda261caafed
xXHeloisaLimaXx <Helo.araujw@gmail.com>
2024-11-17 15:27:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,51 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void povoa_historiograma(char* h[], int valor, int categoria){
-    //povoar com * a categoria com seu respectivo valor\\
-    
-    for(int i = 0; i < valor; i++){
+void povoa_historiograma(char** h, int valores[], int categoria) {
+    // Povoa a categoria com '*' correspondente ao valor
+    for (int i = 0; i < valores[categoria]; i++) {
         h[categoria][i] = '*';
-
-}
-}
-
-void imprimir_historiagrama(char* h[], int valores[], int categorias){
-    
-    for(int i = 0; i < categorias; i++){
-    int j;
-        printf("%d",valores[i]);
-        for(int j = 0; j < valores[i]; j++){
-        printf("%c", h[i][j]);
     }
-    printf("\n");
-        
-    }
-    
 }
 
-int main(){
+void imprimir_historiograma(char** h, int valores[], int categorias) {
+    for (int i = 0; i < categorias; i++) {
+        printf("%d", valores[i]);
+        for (int j = 0; j < valores[i]; j++) {
+            printf("%c", h[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
     int categorias;
     scanf("%d", &categorias);
-    
-    int valores[categorias];
-    char *historiograma[categorias];
-    
-    for(int i = 0; i < categorias; i++){
-        scanf("%d", &valores[i]);
-        
-        if(valores[i] == 0){
-            historiograma[i] = NULL;
-        }else{
-            historiograma[i] = (char *)malloc(valores[i] * sizeof(char));
-            povoa_historiograma(historiograma,valores[i], i);
-            }
-            
-        }
-        imprimir_historiagrama(historiograma, valores, categorias);
-for(int i = 0; i < categorias; i++)
-if (historiograma[i] !=NULL)
-free(historiograma[i]);
 
-        return 0 ;
+    int valores[categorias];
+    char* historiograma[categorias];
+
+    for (int i = 0; i < categorias; i++) {
+        scanf("%d", &valores[i]);
+
+        if (valores[i] == 0) {
+            historiograma[i] = NULL;
+        } else {
+            // Aloca memória suficiente para o número de asteriscos
+            historiograma[i] = (char*)malloc(valores[i] * sizeof(char));
+            povoa_historiograma(historiograma, valores, i);
+        }
     }
+
+    imprimir_historiograma(historiograma, valores, categorias);
+
+    // Libera a memória alocada
+    for (int i = 0; i < categorias; i++) {
+        if (historiograma[i] != NULL)
+            free(historiograma[i]);
+    }
+
+    return 0;
+}
