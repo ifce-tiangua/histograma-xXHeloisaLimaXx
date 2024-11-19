7374bc624bab5e1bad594b5545db84c2c9069b01
xXHeloisaLimaXx <Helo.araujw@gmail.com>
2024-11-19 17:06:22 -0300

    Atualizar o main.c

--- main.c
+++ main.c
@@ -1,18 +1,23 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void exibir_histograma(char* categorias[], int quantidades[], int total) {
+void imprime_histograma(char* categorias[], int quantidades[], int total) {
+
     for (int i = 0; i < total; i++) {
+
         printf("%d ", quantidades[i]);
+
         for (int j = 0; j < quantidades[i]; j++) {
+
             printf("%c", categorias[i][j]);
         }
+
         printf("\n");
     }
 }
 
 void preencher_histograma(char *categorias[], int quantidade, int indice) {
+
     for (int i = 0; i < quantidade; i++) {
         if (categorias[indice][i] >= 0) {
             categorias[indice][i] = '*';
@@ -24,6 +29,38 @@ void preencher_histograma(char *categorias[], int quantidade, int indice) {
 }
 
 int main() {
+
     int total_categorias;
 
-    scanf("%d",
+    scanf("%d", &total_categorias);
+
+    if (total_categorias <= 0) {
+        printf("vazio\n");
+        return 0;
+    }
+
+    int valores[total_categorias];
+    char* histograma[total_categorias];
+
+    for (int i = 0; i < total_categorias; i++) {
+
+        scanf("%d", &valores[i]);
+
+        if (valores[i] == 0) {
+
+            histograma[i] = NULL;
+
+        } else {
+            histograma[i] = (char*)malloc((valores[i] + 1) * sizeof(char));
+
+            preencher_histograma(histograma, valores[i], i);
+        }
+    }
+
+    imprime_histograma(histograma, valores, total_categorias);
+
+    for (int i = 0; i < total_categorias; i++) {
+        free(histograma[i]);
+    }
+    return 0;
+}
